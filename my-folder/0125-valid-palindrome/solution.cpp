ee5e8b5253c7df099df6e865d898b986e214d658
Aritra Chatterjee <147392149+Aritra-Chats@users.noreply.github.com>
2024-05-04 10:12:21 +0000

    [LeetCode Sync] Runtime - 6 ms (6.65%), Memory - 9.1 MB (99.97%)

--- my-folder/0125-valid-palindrome/solution.cpp
+++ my-folder/0125-valid-palindrome/solution.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(string str) {
+    string check = "";
+    int count=0;
     for(int i=0;i<str.length();i++) {
-        if((str[i]<'A' || str[i] > 'Z') && (str[i] < 'a' || str[i] > 'z') && (str[i] < '0' || str[i] > '9')) {
-            str.erase(i,1);
-            i--;
+            if(str[i] >= '0' && str[i] <= '9') check.push_back(str[i]);
+            else {
+                if(str[i]>='a' && str[i]<='z') check.push_back(str[i]);
+                else if(str[i]>='A' && str[i]<='Z') {
+                    str[i] += 32;
+                    check.push_back(str[i]);
+                }
+            }
         }
-    }
-    for(int i=0;i<str.length();i++) {
-        if(str[i]>='A' && str[i]<='Z') {
-            str[i]+=32;
-        }
-    }
-    int n = str.length(),count=0;
-    for(int i =0;i<n;i++) {
-        if(str[i]!=str[n-1-i]) {
+    int n = check.length();
+    for(int i=0;i<n;i++) {
+        if(check[i]!=check[n-1-i]) {
             count++;
         }
     }
