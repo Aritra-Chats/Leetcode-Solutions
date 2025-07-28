0a4002a2c5c03de6c3af63a9b461702bcbc486a6
Aritra Chatterjee <147392149+Aritra-Chats@users.noreply.github.com>
2025-07-28 15:38:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.3 MB (99.98%)

--- my-folder/0349-intersection-of-two-arrays/solution.java
+++ my-folder/0349-intersection-of-two-arrays/solution.java
@@ -1,13 +1,17 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        List<Integer> intersection = new ArrayList<>();
-        for(int i = 0; i < nums1.length; i++) {
-            if(i < nums1.length-1 && nums1[i] == nums1[i+1]) continue;
-            for(int j = 0; j < nums2.length; j++) {
-                if(j < nums2.length-1 && nums2[j] == nums2[j+1]) continue;
-                if(nums1[i] == nums2[j] && !intersection.contains(nums1[i])) intersection.add(nums1[i]);
+        boolean[] map = new boolean[1001];
+        for(int num : nums1) {
+            map[num] = true;
+        }
+        int[] temp = new int[Math.min(nums1.length, nums2.length)];
+        int index = 0;
+        for(int num : nums2) {
+            if(map[num]) {
+                temp[index++] = num;
+                map[num] = false;
             }
         }
-        return intersection.stream().mapToInt(Integer::intValue).toArray();
+        return Arrays.copyOf(temp, index);
     }
 }
