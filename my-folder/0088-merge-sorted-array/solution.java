2ea5d90e73edcbbbf8af26f46f5667c749f41599
Aritra Chatterjee <147392149+Aritra-Chats@users.noreply.github.com>
2025-08-12 10:39:45 +0000

    [LeetCode Sync] Runtime - 1 ms (22.31%), Memory - 42.3 MB (99.95%)

--- my-folder/0088-merge-sorted-array/solution.java
+++ my-folder/0088-merge-sorted-array/solution.java
@@ -1,15 +1,14 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] copy = Arrays.copyOf(nums1, m);
-        int index = 0, i = 0, j = 0; 
+        int i = 0, j = 0; 
         while(i < m && j < n) {
-            if(copy[i] <= nums2[j]) {
-                if(copy[i] == nums2[j]) nums1[index++] = nums2[j++];
-                nums1[index++] = copy[i++];
+            if(nums2[j] <= nums1[i]) {
+                for(int k = nums1.length-1; k > i; k--) nums1[k] = nums1[k-1]; 
+                nums1[i++] = nums2[j++];
+                m++;
             }
-            else nums1[index++] = nums2[j++];
+            else i++;
         }
-        while(i < m) nums1[index++] = copy[i++];
-        while(j < n) nums1[index++] = nums2[j++];
+        while(j < n) nums1[i++] = nums2[j++];
     }
 }
