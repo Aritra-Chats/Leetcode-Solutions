e35a6b2899df21c548bb512837e9cc8620efea3e
Aritra Chatterjee <147392149+Aritra-Chats@users.noreply.github.com>
2025-09-30 11:37:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.8 MB (99.97%)

--- my-folder/0094-binary-tree-inorder-traversal/solution.java
+++ my-folder/0094-binary-tree-inorder-traversal/solution.java
@@ -16,6 +16,30 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         
+        // ---Morris---
+        List<Integer> result = new ArrayList<>();
+        TreeNode curr = root;
+        while(curr != null) {
+            if(curr.left == null) {
+                result.add(curr.val);
+                curr = curr.right;
+            } else {
+                TreeNode next = curr.left;
+                while(next.right != null && next.right != curr) 
+                    next = next.right;
+                if(next.right == null) {
+                    next.right = curr;
+                    curr = curr.left;
+                } else {
+                    next.right = null;
+                    result.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
+        return result;
+
+
         // ---Iteractive---
         /*List<Integer> elems = new ArrayList<>();
         if(root == null) return elems;
@@ -25,7 +49,7 @@ class Solution {
         return elems;*/
 
         // ---Recursive---
-        List<Integer> result = new ArrayList<>();
+        /*List<Integer> result = new ArrayList<>();
         if(root == null) return result;
         List<TreeNode> stack = new ArrayList<>();
         int top = -1;
@@ -50,6 +74,6 @@ class Solution {
                 }
             }
         }
-        return result;
+        return result;*/
     }
 }
