c50363d4a5e3f041caa358ffdda37916a1426145
Aritra Chatterjee <147392149+Aritra-Chats@users.noreply.github.com>
2025-12-28 06:13:07 +0000

    [LeetCode Sync] Runtime - 18 ms (31.83%), Memory - 48.1 MB (12.83%)

--- my-folder/1171-shortest-path-in-binary-matrix/solution.java
+++ my-folder/1171-shortest-path-in-binary-matrix/solution.java
@@ -1,17 +1,10 @@
 class Solution {
-    private class Pair {
-        int r, c;
-        private Pair(int row, int col) {
-            this.r = row;
-            this.c = col;
-        }
-    }
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
-        Queue<Pair> q = new ArrayDeque<>();
+        Queue<int[]> q = new ArrayDeque<>();
         boolean[][] vis = new boolean[n][n];
-        q.offer(new Pair(0, 0));
+        q.offer(new int[]{0, 0});
         vis[0][0] = true;
         int[] dr = {0, 1, 0, -1, 1, 1, -1, -1};
         int[] dc = {1, 0, -1, 0, 1, -1, 1, -1};
@@ -19,15 +12,15 @@ class Solution {
         while(!q.isEmpty()) {
             int size = q.size();
             for(int i = 0; i < size; i++) {
-                Pair node = q.poll();
-                if(node.r == n-1 && node.c == n-1) 
+                int[] node = q.poll();
+                if(node[0] == n-1 && node[1] == n-1) 
                     return dist;
                 for(int j = 0; j < 8; j++) {
-                    if(node.r + dr[j] < 0 || node.r + dr[j] >= grid.length || node.c + dc[j] < 0 || node.c + dc[j] >= grid.length)
+                    if(node[0] + dr[j] < 0 || node[0] + dr[j] >= grid.length || node[1] + dc[j] < 0 || node[1] + dc[j] >= grid.length)
                         continue;
-                    if(grid[node.r + dr[j]][node.c + dc[j]] == 0 && !vis[node.r + dr[j]][node.c + dc[j]]) {
-                        q.offer(new Pair(node.r + dr[j], node.c + dc[j]));
-                        vis[node.r + dr[j]][node.c + dc[j]] = true;
+                    if(grid[node[0] + dr[j]][node[1] + dc[j]] == 0 && !vis[node[0] + dr[j]][node[1] + dc[j]]) {
+                        q.offer(new int[]{node[0] + dr[j], node[1] + dc[j]});
+                        vis[node[0] + dr[j]][node[1] + dc[j]] = true;
                     }
                 }
             }
