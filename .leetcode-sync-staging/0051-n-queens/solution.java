5bdf31f8f0a33caa3beedb0eafbd0b621f21c302
Aritra Chatterjee <147392149+Aritra-Chats@users.noreply.github.com>
2026-05-08 12:49:38 +0000

    [LeetCode Sync] Runtime - 2 ms (81.45%), Memory - 46.2 MB (95.52%)

---
+++ .leetcode-sync-staging/0051-n-queens/solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    private boolean isValid(int row, int col, int[] assigned) {
+        for(int prevRow = 0; prevRow < row; prevRow++) {
+            int prevCol = assigned[prevRow];
+            if(prevRow == row || prevCol == col || Math.abs(row - prevRow) == Math.abs(col - prevCol))
+                return false;
+        }
+        return true;
+    }
+    private void assignQueens(int row, int prev, String[] rows, int n, List<String> iterator, List<List<String>> queens, int[] assigned) {
+        if(row == n) {
+            queens.add(new ArrayList<>(iterator));
+        }
+        for(int col = 0; col < n; col++) {
+            if(!isValid(row, col, assigned))
+                continue;
+            iterator.add(rows[col]);
+            assigned[row] = col;
+            assignQueens(row + 1, row, rows, n, iterator, queens, assigned);
+            assigned[row] = -1;
+            iterator.remove(iterator.size() - 1);
+        }
+    }
+    public List<List<String>> solveNQueens(int n) {
+        String[] rows = new String[n];
+        for(int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j < n; j++) 
+                sb.append((j == i) ? 'Q' : '.');
+            rows[i] = sb.toString();
+        }
+        List<List<String>> queens = new ArrayList<>();
+        int[] assigned = new int[n];
+        Arrays.fill(assigned, -1);
+        assignQueens(0, -1, rows, n, new ArrayList<>(), queens, assigned);
+        return queens;
+    }
+}
