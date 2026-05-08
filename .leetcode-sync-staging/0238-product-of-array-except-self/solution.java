e3f86337978627bafeff22e034ab9ae79f0f48c1
Aritra Chatterjee <147392149+Aritra-Chats@users.noreply.github.com>
2026-05-08 05:04:36 +0000

    [LeetCode Sync] Runtime - 2 ms (95.19%), Memory - 64.9 MB (85.91%)

---
+++ .leetcode-sync-staging/0238-product-of-array-except-self/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int size = nums.length;
+        int[] prefix = new int[size];
+        prefix[0] = 1;
+        for(int i = 1; i < size; i++)
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        int[] suffix = new int[size];
+        suffix[size - 1] = 1;
+        for(int i = size - 2; i >= 0; i--)
+            suffix[i] = suffix[i + 1] * nums[i + 1];
+        int[] res = new int[size];
+        for(int i = 0; i < size; i++)
+            res[i] = prefix[i] * suffix[i];
+        return res;
+    }
+}
