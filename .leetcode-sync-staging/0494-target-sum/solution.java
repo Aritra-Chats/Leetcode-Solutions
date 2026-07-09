08a0bdd8d43b2163ad0f0e041bee56d62450bd28
Aritra-Chats <147392149+Aritra-Chats@users.noreply.github.com>
2026-07-09 07:32:11 +0000

    [LeetCode Sync] Runtime - 4 ms (87.88%), Memory - 43.5 MB (55.08%)

--- .leetcode-sync-staging/0494-target-sum/solution.java
+++ .leetcode-sync-staging/0494-target-sum/solution.java
@@ -3,17 +3,16 @@ class Solution {
         int len = nums.length, totalSum = 0;
         for(int num : nums)
             totalSum += num;
-        if(Math.abs(target) > totalSum || (target + totalSum) % 2 != 0)
+        if(Math.abs(target) > totalSum || (target+totalSum) % 2 != 0)
             return 0;
         int actarget = (target + totalSum) / 2;
-        int[] prev = new int[actarget+1];
-        prev[0] = 1;
+        int[] dp = new int[actarget+1];
+        dp[0] = 1;
         for(int i = 1; i <= len; i++) {
-            int[] temp = new int[actarget+1];
-            for(int val = 0; val <= actarget; val++)
-                temp[val] = (nums[i-1] <= val) ? prev[val] + prev[val-nums[i-1]] : prev[val];
-            prev = temp; 
+            for(int val = actarget; val >= 0; val--) {
+                dp[val] = (nums[i-1] <= val) ? dp[val] + dp[val-nums[i-1]] : dp[val];
+            }
         }
-        return prev[actarget];
+        return dp[actarget];
     }
 }
