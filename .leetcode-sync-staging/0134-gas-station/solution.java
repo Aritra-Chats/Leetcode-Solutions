e82e3871013780385862c9411e797050398ba8a1
Aritra-Chats <147392149+Aritra-Chats@users.noreply.github.com>
2026-08-11 20:05:21 +0000

    [LeetCode Sync] Runtime - 2 ms (99.18%), Memory - 119.3 MB (65.66%)

---
+++ .leetcode-sync-staging/0134-gas-station/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int len = gas.length, tank = 0, start = 0, total = 0;
+        for(int i = 0; i < len; i++) {
+            int diff = gas[i] - cost[i];
+            total += diff;
+            tank += diff;
+            if(tank < 0) {
+                start = i+1;
+                tank = 0;
+            }
+        }
+        return (total < 0) ? -1 : start;
+    }
+}
