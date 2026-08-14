85af20765f8f5a29dba167efd908fa8eddd0c3d9
Aritra-Chats <147392149+Aritra-Chats@users.noreply.github.com>
2026-08-14 18:38:44 +0000

    [LeetCode Sync] Organize synced solutions into DSA and SQL

--- DSA/binary-search/0907-koko-eating-bananas/solution.java
+++ DSA/binary-search/0907-koko-eating-bananas/solution.java
@@ -1,26 +1,23 @@
 class Solution {
-    public boolean isPossible(int[] piles, int h, int k) {
-        int totalTime = 0;
-        for(int bananas : piles) {
-            totalTime += (bananas + k - 1) / k;
-        }
-        return totalTime <= h;
+    private boolean canFinishBananasInTime(int mid, int[] piles, int h) {
+        long time = 0;
+        for(int pile : piles) 
+            time += (pile +(long) mid - 1) / mid;
+        return time <= h;
     }
-
     public int minEatingSpeed(int[] piles, int h) {
-        int max = 0;
-        for(int bananas : piles) {
-            max = Math.max(max, bananas);
+        int low = 1, high = 0;
+        for(int pile : piles) {
+            if(pile > high)
+                high = pile;
         }
-        int l = 1, hi = max;
-        while(l < hi) {
-            int m = l + (hi-l)/2;
-            if(isPossible(piles, h, m)) {
-                hi = m;
-            }
-            else 
-                l = m+1;
+        while(high - low > 0) {
+            int mid = low + (high - low) / 2;
+            if(canFinishBananasInTime(mid, piles, h)) 
+                high = mid;
+            else
+                low = mid+1;
         }
-        return l;
+        return high;
     }
 }
