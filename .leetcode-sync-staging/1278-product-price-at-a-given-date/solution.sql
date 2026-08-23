6e777a88eb774d12b2c980981f2282688c4fa4d6
Aritra-Chats <147392149+Aritra-Chats@users.noreply.github.com>
2026-08-23 08:27:24 +0000

    [LeetCode Sync] Runtime - 507 ms (63.96%), Memory - 0.0B (100.00%)

---
+++ .leetcode-sync-staging/1278-product-price-at-a-given-date/solution.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT p.product_id, p.new_price AS price 
+FROM Products p
+INNER JOIN (
+    SELECT product_id, MAX(change_date) AS change_date
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+) latest
+ON p.product_id = latest.product_id AND p.change_date = latest.change_date
+UNION
+SELECT product_id, 10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16';
